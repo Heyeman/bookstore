c5947acd9f198ac0759e636812820e3dd40cb772
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-06-02 17:33:04 +0300

    Add files via upload

---
+++ routes/authRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+
+// Auth/login and auth/register - Post
+// auth/profile - GET – show profile
+// auth/profile - PUT - update profile
---
+++ routes/bookRoutes.js
@@ -0,0 +1,5 @@
+// Books - GET -  with/without query param
+// Books - POST
+// Books/:id - GET - one book details and reviews
+// Books/:id - DELETE/PUT - delete/update books
+// books/:id/review - POST
