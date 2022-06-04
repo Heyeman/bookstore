4f12dc0b8c899e9ba442a36139b6dc02538eac7a
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-06-04 13:52:00 +0300

    Auth initial setup

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,16 @@
 const express = require("express");
 
+const router = express.Router();
+
 // Auth/login and auth/register - Post
+router.post("/login", loginController);
+router.post("/register", signupController);
+
 // auth/profile - GET – show profile
 // auth/profile - PUT - update profile
+router
+  .route("/profile")
+  .get(protector, myProfile)
+  .put(protector, updateProfile);
+
+module.exports = router;
