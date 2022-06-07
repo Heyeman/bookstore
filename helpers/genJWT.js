420571e804ed0098c77593515494f6c60a7e89a3
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-06-07 15:11:22 +0300

    completed the registration endpoint

---
+++ helpers/genJWT.js
@@ -0,0 +1,7 @@
+const jwt = require("jsonwebtoken");
+
+const getToken = (id, isRefreshToken = false) => {
+  let duration = isRefreshToken ? "1y" : "15m";
+  return jwt.sign(id, process.env.JWT_SECRET, { expiresIn: duration });
+};
+module.exports = { getToken };
