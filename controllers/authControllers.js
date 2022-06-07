d713c3c230e0c8e9ee406b8e42cbdfce09848576
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-06-07 15:12:38 +0300

    completed registration endpoint - left to add some files

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
 const { User } = require("../models/userModel");
+const { getToken } = require("../helpers/genJWT");
+
 //login controller
 const loginController = asyncHandler(async (req, res) => {
   //required params - email and password
@@ -25,13 +27,15 @@ const signupController = asyncHandler(async (req, res) => {
   } = req.body;
   if (!(email && password && username)) {
     res.status(400);
-    throw new Error("Both email and password should be filled");
+    throw new Error("Email, username and password should be filled");
   }
-  const existingUser = await User.find({ email });
+  const existingUser = await User.findOne({ email });
   if (existingUser) {
     res.status(400);
     throw new Error("User already exists");
   }
+  const hashSalt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(password, hashSalt);
   const newUser = await User.create({
     email,
     password,
@@ -41,6 +45,21 @@ const signupController = asyncHandler(async (req, res) => {
     phoneNumber,
     whatsapp,
   });
+  if (newUser) {
+    const accessToken = await getToken({ id: newUser._id });
+    const refreshToken = await getToken({ id: newUser._id }, true);
+
+    res.status(200).json({
+      id: newUser._id,
+      username,
+      email,
+      accessToken,
+      refreshToken,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Faulty user data entered");
+  }
 });
 //show profile
 const myProfile = asyncHandler(async (req, res) => {});
