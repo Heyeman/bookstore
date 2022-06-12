f82a6539953ed35f315ba1533e087733e61c08ad
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-06-12 21:26:19 +0300

    route protector middleware setup:

--- helpers/genJWT.js
+++ helpers/genJWT.js
@@ -2,6 +2,6 @@ const jwt = require("jsonwebtoken");
 
 const getToken = (id, isRefreshToken = false) => {
   let duration = isRefreshToken ? "1y" : "15m";
-  return jwt.sign(id, process.env.JWT_SECRET, { expiresIn: duration });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: duration });
 };
 module.exports = { getToken };
--- middlewares/protector.js
+++ middlewares/protector.js
@@ -1,6 +1,25 @@
 const asyncHandler = require("express-async-handler");
-module.exports = asyncHandler(async (req, res) => {
-  //required params - email and password
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/userModel");
 
-  res.send("protector");
+module.exports = asyncHandler(async (req, res, next) => {
+  const bearer = req.headers.authorization;
+  if (!bearer || !bearer.startsWith("Bearer")) {
+    res.status(401);
+    throw new Error("Unauthorized");
+  }
+  const token = bearer.split(" ")[1];
+  try {
+    const decode = await jwt.verify(token, process.env.JWT_SECRET);
+    const userExists = await User.findById(decode.id).select("-password");
+    req.user = userExists;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      res.status(403);
+      throw new Error("Expired token");
+    }
+    res.status(401);
+    throw new Error("Unauthorized");
+  }
 });
