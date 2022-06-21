b104c6ce08d70741de2e378ef74cb0c8fbd3842b
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-06-21 12:02:19 +0300

    mongo uri

--- config/dbConn.js
+++ config/dbConn.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.DB);
+    await mongoose.connect(process.env.DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Database connected successfully".cyan.underline);
   } catch (err) {
-    console.log("Can't connect to the database with the link".red);
+    console.log("Can't connect to the database with the link".red, err);
   }
 };
 
